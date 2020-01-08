f09e50d22bb31d95d437b5585c92ee0b82ed300d
MikkoHuttunen <l4757@student.jamk.fi>
2020-01-08 14:05:29 +0200

    Harjoitus 13 C kesken

--- harjoitukset13-22_mikko_huttunen/Harjoitus 13/modules/modarun.js
+++ harjoitukset13-22_mikko_huttunen/Harjoitus 13/modules/modarun.js
@@ -3,4 +3,4 @@ const modb = require('./modb.js');
 const testarray = [1, 2, 3, 4, 5, 6];
 
 console.log(moda(290, 300));
-console.log(modb(290, 300, testarray));
+console.log(new modb(290, 300, testarray));
---
+++ harjoitukset13-22_mikko_huttunen/Harjoitus 13/modules/modc.js
@@ -0,0 +1,22 @@
+class AvgNumber {
+    constructor(min, max, array) {
+        this.min = min;
+        this.max = max;
+        this.array = array;
+    }
+
+    randomNumber() {
+        return Math.floor(this.min + Math.random() * ((this.max - this.min) + 1));
+    }
+
+    calcAverage() {
+        let sum = 0;
+        for (let i = 0; i < this.array.length; i++) {
+            sum += this.array[i];
+        }
+        const avg = sum/this.array.length;
+        return avg;
+    }
+};
+
+module.exports = new AvgNumber(100, 200);
