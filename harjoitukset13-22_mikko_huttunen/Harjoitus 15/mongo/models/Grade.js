55599db03977e721025f99f3b4fac0072aa688d0
MikkoHuttunen <l4757@student.jamk.fi>
2020-02-04 14:53:27 +0200

    Harjoitus 15 tehty

--- harjoitukset13-22_mikko_huttunen/Harjoitus 15/mongo/models/Grade.js
+++ harjoitukset13-22_mikko_huttunen/Harjoitus 15/mongo/models/Grade.js
@@ -1,8 +1,9 @@
 let mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-let GradeSchema = new mongoose.Schema({
-    coursecode: String,
-    grade: Number,
+let Grade = new Schema({
+    coursecode: { type: String, required: true, max: 50 },
+    grade: { type: Number, required: false, min: 0, max: 5 },
 });
 
-//module.exports = mongoose.model('Grade', GradeSchema);
+module.exports = Grade;
--- harjoitukset13-22_mikko_huttunen/Harjoitus 15/mongo/models/Student.js
+++ harjoitukset13-22_mikko_huttunen/Harjoitus 15/mongo/models/Student.js
@@ -1,14 +1,13 @@
 let mongoose = require('mongoose');
+const Grade = require('./Grade');
 
 let students = new mongoose.Schema({
-    studentcode: String,
-    name: String,
-    email: String,
-    studypoints: Number,
-    grades: [{
-        coursecode: String,
-        grade: Number,
-    }],
+    studentcode: { type: String, unique: true, required: true, match: /[a-z]{1}[0-9]{4}/ },
+    name: { type: String, required: true, max: 80 },
+    email: { type: String, unique: true, required: true, max: 60 },
+    studypoints: { type: Number, required: false, min: 0, max: 300 },
+    grades: { type: [Grade], required: true },
 });
 
-module.exports = mongoose.model('Students', students);
+const StudentModel = mongoose.model('Student', students);
+module.exports = StudentModel;
